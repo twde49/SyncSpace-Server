110f81ee32728ce8cb6d137258f63ac142223198
Thibaut <thibautstachnick@gmail.com>
2025-03-10 17:00:57 +0100

    add notification drawer and some more

---
+++ src/Controller/NotificationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Notification;
+use App\Repository\NotificationRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/notifications')]
+class NotificationController extends AbstractController
+{
+    #[Route('/all', name: 'app_notification_index',methods: ['GET'])]
+    public function index(NotificationRepository $notificationRepository): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $notifications = $notificationRepository->getUnreadNotifications($user);
+        return $this->json($notifications, Response::HTTP_OK, [], ['groups' => ['notification:read']]);
+    }
+    
+    
+    #[Route('/new', name: 'app_notification_create',methods: ['POST'])]
+    public function createNotification(Request $request, UserRepository $userRepository, EntityManagerInterface $manager):Response
+    {
+        $data = json_decode($request->getContent(), true);
+        
+        $notification = new Notification();
+        $notification->setTitle($data['title']);
+        $notification->setContent($data['content']);
+        $relatedToUser = $userRepository->find($data['userId']);
+        $notification->setRelatedTo($relatedToUser);    
+        $manager->persist($notification);
+        $manager->flush();
+        return $this->json($notification, Response::HTTP_CREATED, [], ['groups' => ['notification:read']]);
+    }
+    
+    #[Route('/{id}/read', name: 'app_notification_read',methods: ['PUT'])]
+    public function readNotification(Notification $notification, EntityManagerInterface $manager): Response
+    {
+        $notification->setRead(true);
+        $manager->flush();
+        return $this->json($notification, Response::HTTP_OK, [], ['groups' => ['notification:read']]);
+    }
+}
---
+++ src/Entity/Notification.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NotificationRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
+
+#[ORM\Entity(repositoryClass: NotificationRepository::class)]
+class Notification
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['notification:read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['notification:read'])]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['notification:read'])]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    #[Groups(['notification:read'])]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    #[Groups(['notification:read'])]
+    private ?bool $isRead = null;
+
+    #[ORM\ManyToOne(inversedBy: 'notifications')]
+    #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['notification:read'])]
+    private ?User $relatedTo = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->isRead = false;
+    }
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function isRead(): ?bool
+    {
+        return $this->isRead;
+    }
+
+    public function setRead(bool $isRead): static
+    {
+        $this->isRead = $isRead;
+
+        return $this;
+    }
+
+    public function getRelatedTo(): ?User
+    {
+        return $this->relatedTo;
+    }
+
+    public function setRelatedTo(?User $relatedTo): static
+    {
+        $this->relatedTo = $relatedTo;
+
+        return $this;
+    }
+}
